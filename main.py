a75f971a31994b85daac5033f357a0aa1c6f94cf
eyupatahanli <eyupatahanli@gmail.com>
2022-11-07 05:36:09 +0300

    dosya yolu silindi

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("/Users/eyupburakatahanli/Desktop/veri_madenciligi/PM2.5 Global Air Pollution 2010-2017.csv")
+df = pd.read_csv("")
