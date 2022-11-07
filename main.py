e5b56b09c04c5bc5232744a5d680239fd0e05276
eyupatahanli <eyupatahanli@gmail.com>
2022-11-07 05:34:37 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#kütüphanelerin import edilmesi
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("/Users/eyupburakatahanli/Desktop/veri_madenciligi/PM2.5 Global Air Pollution 2010-2017.csv")
